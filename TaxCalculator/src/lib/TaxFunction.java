48b701ed570c8021d0005f43c347f707605e66ea
bintangrayhan <bintangrayhan7@gmail.com>
2024-04-21 03:47:09 +0700

    Menghilangkan Logika yang Berulang dan Mencegah Kesalahan
    Terdapat duplikasi logika dalam menghitung pajak berdasarkan status pernikahan dan jumlah anak, yang bisa dioptimalkan untuk mengurangi kode yang berulang. dan  Tanpa batasan ini, ada potensi untuk menghitung pajak dengan nilai numberOfChildren yang tidak realistis atau valid, yang dapat menyebabkan kesalahan atau hasil yang tidak akurat.

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -10,27 +10,20 @@ public class TaxFunction {
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
-		int tax = 0;
+		
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
+        numberOfChildren = Math.min(numberOfChildren, 3);
+
+        int nonTaxableIncome = NON_TAXABLE_INCOME + (isMarried ? MARRIED_ADDITIONAL : 0) + (numberOfChildren * CHILD_ADDITIONAL);
+
+        int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible - nonTaxableIncome;
+        int tax = (int) Math.round(0.05 * taxableIncome);
+
+        return Math.max(tax, 0);
 			 
 	}
 	
